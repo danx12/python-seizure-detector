28e46a533bef99ec71ab8de3a7431e2dc37e8433
Daniel Villarreal <daniel.villarreal12@gmail.com>
2016-11-26 18:58:25 -0700

    Added to support linearSVC.

--- classification.py
+++ classification.py
@@ -15,23 +15,33 @@ import multiprocessing as mp
 import tabulate as tab
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report,confusion_matrix
+
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 
 #%%
-eeg_feat = readStoredData('cc_automatic.p')
-X = eeg_feat['feats']
-y = eeg_feat['labels']
+eeg_train = readStoredData('cc_automatic.p')
+X = eeg_train['feats']
+y = eeg_train['labels']
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=0)
+eeg_alt = readStoredData('eeg_pat22_feats.p')
+X_alt = eeg_alt['feats']
+y_alt = np.ravel(eeg_alt['labels'])
 
-#tuned_parameters = [{'kernel':['rbf'],'gamma':[1e0,1e-1,1e-2,1e-3,1e-4,1e-5],'C':[1,10,100,1000]},
-#                    {'kernel':['linear'],'C':[1,10,100,1000]}]
 
-tuned_parameters = [{'kernel':['linear'],'C':[1,10,100]}]
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=41)
+
+#tuned_parameters = [{'kernel':['linear'],'C':[1,10,100,1000]}
+#                    ,
+#                    {'kernel':['rbf'],'gamma':[1e0,1e-1,1e-2,1e-3,1e-4,1e-5],'C':[1,10,100,1000]}
+#                    ]
+
+tuned_parameters = [{'C':[1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,0.1,0.5,1,10,100,1000,2000,3000,4000,5000,7000,9000,10000]}]
                     
-scores = ['precision','recall','roc_auc']
+#scores = ['precision_macro','recall_macro','roc_auc']
+#scores = ['f1','f1_macro']
+scores = ['roc_auc']
 
 #f1
 
@@ -41,8 +51,8 @@ for score in scores:
     print("# Tuning hyper-parameters for %s" % score)
     print()
 
-    clf = GridSearchCV(SVC(class_weight='balanced',cache_size=700), tuned_parameters, cv=3,scoring='%s_macro' % score,
-                       n_jobs=-1,verbose=5)
+    clf = GridSearchCV(LinearSVC(class_weight='balanced',random_state=41), tuned_parameters, cv=10,scoring='%s' % score,
+                       n_jobs=-1,verbose=0)
     clf.fit(X_train, np.ravel(y_train))
 
     print("Best parameters set found on development set:")
@@ -65,4 +75,7 @@ for score in scores:
     print()
     y_true, y_pred = y_test, clf.predict(X_test)
     print(classification_report(y_true, y_pred))
+    print("Confusion matrix")
+    print(confusion_matrix(y_true,y_pred))
     print()
+    cf = np.ravel(clf.best_estimator_.coef_)
